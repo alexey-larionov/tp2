24893a46a4f676fd16a045d5d9645e48e92bb2fe
Larionov <alexey_larionov@hotmail.com>
2022-03-19 22:09:20 +0000

    Exported functions and added license

--- R/dot_product.R
+++ R/dot_product.R
@@ -3,9 +3,8 @@
 #' @author Alexey Larionov
 #' @param x scalar, vector or matrix
 #' @param y scalar, vector or matrix
-#' @return The dot product of \code(x) and \code(y)
-#' @examples
-#' product_by_element(c(1,2,3),matrix(c(1:12), nrow=3)
+#' @return The dot product of x and y
+#' @export dot_product
 dot_product <- function(x,y){
   z <- x %*% y
   return(z)
--- R/product_by_element.R
+++ R/product_by_element.R
@@ -1,11 +1,10 @@
-#' Element-wise product
+#' Product by element
 #'
 #' @author Alexey Larionov
 #' @param x scalar, vector or matrix
 #' @param y scalar, vector or matrix
-#' @return The element-wise product of \code(x) and \code(y)
-#' @examples
-#' product_by_element(c(1,2,3),c(4,5,6))
+#' @return The element-wise product of x and y
+#' @export product_by_element
 product_by_element <- function(x,y){
   z <- x * y
   return(z)
